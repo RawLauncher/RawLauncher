abcbd92ff69b443ffa8c46b4a4144d6ff2844b5a
Satyan JACQUENS <satyan.jacquens@grenoble-inp.org>
2017-08-24 15:31:19 +0200

    Create a basic working structure

---
+++ app/src/main/java/com/sjcqs/rawlauncher/MainManager.java
@@ -0,0 +1,9 @@
+package com.sjcqs.rawlauncher;
+
+/**
+ * Created by satyan on 8/23/17.
+ * Manager for interraction between the app objects
+ */
+
+public class MainManager {
+}
---
+++ app/src/main/java/com/sjcqs/rawlauncher/UIManager.java
@@ -0,0 +1,9 @@
+package com.sjcqs.rawlauncher;
+
+/**
+ * Created by satyan on 8/23/17.
+ * Handle the UI changes
+ */
+
+public class UIManager {
+}
